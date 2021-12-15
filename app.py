b5c756b928a14cb3a9920d25489bae9a3cb85448
yawsonsamuel320 <yawsonsamuel320@gmail.com>
2021-12-15 02:48:57 +0000

    set up for deployment

---
+++ app.py
@@ -0,0 +1,94 @@
+import streamlit as st
+import requests
+from bs4 import BeautifulSoup
+
+import pandas as pd
+import numpy as np
+
+
+from format import *
+
+from scrape import return_approved
+from scrape import name_drug
+from scrape import generate_dan
+from scrape import return_img_url
+
+
+configure_page()
+tab = toggle_sidebar()
+
+
+if tab == "Home":
+    #st.header("Drug Repurposing Navigator")
+    display_logo()
+    display_header(tab)
+    
+    cancers_list = [part + " Cancer" for part in ["Breast", "Ovarian", "Pancreatic", "Prostate"]]
+    
+                    
+    cancer_type = st.selectbox("Select Cancer Type", cancers_list, key="sourceKey")
+    
+    option = cancer_type.split()[0].lower()
+    
+    url = "https://www.cancer.gov/about-cancer/treatment/drugs/" + option
+    
+    
+    approved_drugs_list = return_approved(url)
+
+    drug = st.selectbox(
+        "Select a drug:", approved_drugs_list
+    )
+    
+    
+    text_contents = '''
+                    Foo, Bar
+                    123, 456
+                    789, 000
+                    '''
+    st.write("\n\n\n")   
+    
+    col1, col2, col3, col4, col5, col6, col7 = st.columns(7)
+    
+    with col4:
+        st.download_button('Download CSV', text_contents, 'text/csv')
+    
+    st.markdown("### Generic Name")
+    st.write(name_drug(drug))
+  
+    dan = generate_dan(drug)
+    
+    img_url = return_img_url(drug)
+    
+    st.markdown("### Chemical Structure")
+    st.markdown(f"""![Alt Text]({img_url})""", unsafe_allow_html=True)
+    
+    st.markdown("### Research Status")
+    st.write(state_drug_status(drug))
+    
+    st.markdown("### Chemical Formula")
+    st.write(return_formula(drug))
+    
+    st.markdown("### Monoisotopic Weight")
+    st.write(f"""{return_weight(drug)}""")
+    
+    st.markdown("### Targets")
+    for target in return_targets(drug):
+        st.write(target)
+        
+        
+    st.markdown("### Enzymes")
+    for enzyme in return_enzymes(drug):
+        st.write(enzyme)
+        
+        
+    st.markdown("### Transporters")
+    for transporters in return_transporters(drug):
+        st.write(transporters)
+        
+    st.markdown("### Similiar Structures")
+    st.dataframe(return_similar(drug))
+    
+    
+    
+    
+    
---
+++ format.py
@@ -0,0 +1,43 @@
+import streamlit as st
+from scrape import *
+from PIL import Image
+
+
+image = Image.open("C:\\Users\\hp\\Documents\\Biomedical Engineering\\Noguchi Internship\\Drug Repurposing\\drug-repurposing\\drug_repurposing\\image\\logo.png")
+
+
+def clear_page():
+    hide_st_style = """
+                        <style>
+                        #MainMenu {visibility: hidden;}
+                        footer {visibility: hidden;}
+                        header {visibility: hidden;}
+                        </style>
+                 """
+    st.markdown(hide_st_style, unsafe_allow_html=True)
+    
+    
+def configure_page():
+    st.set_page_config(page_title="Drug Repurposing", layout="wide", page_icon=image, initial_sidebar_state="collapsed")
+    
+    clear_page()
+
+def center_align(item):
+    col1, col2, col3, col4, col5 = st.columns(5)
+    
+    col3.image(item, width=300)
+    
+
+def display_logo():
+    center_align(image)
+    
+    
+def toggle_sidebar():
+    menu = ["Home", "Next Page"]
+    tab = st.sidebar.selectbox("Drug Repurposing", menu)
+    
+    return tab
+
+def display_header(tab):
+    st.subheader(tab.upper())
+    
